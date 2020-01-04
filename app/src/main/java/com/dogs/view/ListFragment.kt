f3edbc2fe105f031d6adcaefcd8c5e55db9572d0
Piyush <piyush.ibuls@gmail.com>
2020-01-05 01:24:15 +0530

    added a navigation from list fragent to details fragment

--- app/src/main/java/com/dogs/view/ListFragment.kt
+++ app/src/main/java/com/dogs/view/ListFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -23,4 +25,13 @@ class ListFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabCall.setOnClickListener {
+
+            // class ListFragmentDirections is automatically generated..
+            val actionDetailPage = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(actionDetailPage)
+        }
+    }
 }
