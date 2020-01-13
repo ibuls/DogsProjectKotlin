39c4e4d96369bfb81d460f9b0188c058cd0bc7a2
Piyush <piyush.ibuls@gmail.com>
2020-01-13 13:37:56 +0530

    interceptor code added only few changes needed

--- app/src/main/java/com/dogs/model/DogsApiService.kt
+++ app/src/main/java/com/dogs/model/DogsApiService.kt
@@ -1,39 +1,43 @@
 package com.dogs.model
 
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import io.reactivex.Single
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.locks.Lock
 
 class DogsApiService {
 
-    private val BASE_URL = "https://raw.githubusercontent.com/";
-
-
-    private val client:OkHttpClient = OkHttpClient()
-
-    private var api:DogsApi? = null
-    private var Lock = Any()
-
-
-    fun  getApi()=api?: synchronized(Lock){
-        api?{
-           Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-                .create(DogsApi::class.java)
-               .also {
-                   api = it
-               }
-        }
-    }
 
     fun getDogs():Single<List<DogBreed>>{
-        return api.getDogs()
+        return getSingletonClient().getDogs()
+    }
+
+    companion object{
+        @Volatile private var api:DogsApi? = null
+
+
+        private fun getSingletonClient(): DogsApi? {
+
+            if (api==null) {
+                val BASE_URL = "https://raw.githubusercontent.com/";
+
+                val client: OkHttpClient =
+                    OkHttpClient.Builder().addInterceptor(StethoInterceptor()).build()
+                api = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+                    .create(DogsApi::class.java);
+
+            }
+            return api
+
+        }
+
+
     }
 }
