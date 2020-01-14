f38b53680f601d60678923e9437cc543e18149bd
Piyush <piyush.ibuls@gmail.com>
2020-01-15 00:15:02 +0530

    palette library integrated

--- app/src/main/java/com/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/dogs/view/DetailFragment.kt
@@ -1,6 +1,8 @@
 package com.dogs.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,9 +12,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 import com.dogs.R
 import com.dogs.databinding.FragmentDetailBinding
+import com.dogs.model.DogPalette
 import com.dogs.utils.getProgressDrawable
 import com.dogs.utils.loadImage
 import com.dogs.viewmodel.DetailViewModel
@@ -69,10 +76,36 @@ class DetailFragment : Fragment() {
             dog?.let {
                 binding.dog = it
 
+                it.imageUrl?.let {
+
+                    setupBackgroundColor(it)
+                }
+
             }
         })
 
 
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object :CustomTarget<Bitmap>(){
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource).generate { palette ->
+
+                        val intColor = palette?.lightMutedSwatch?.rgb?:0; // if rgb is null then we send 0
+                        val myPallet = DogPalette(intColor)
+                        binding.pelette = myPallet
+                    }
+                }
+
+            })
     }
 
 
