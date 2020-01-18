291954c8173c284199e80f48e4c418c3b4f75a91
Piyush <piyush.ibuls@gmail.com>
2020-01-19 02:32:52 +0530

    Multidex completed...
    And Hence Cource is now Complete!!!

--- app/src/main/java/com/dogs/utils/MyApplication.kt
+++ app/src/main/java/com/dogs/utils/MyApplication.kt
@@ -1,11 +1,23 @@
 package com.dogs.utils
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.facebook.stetho.Stetho
+import com.google.firebase.FirebaseApp
 
-class MyApplication: Application() {
+
+// for multidex issue fix
+class MyApplication: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        Stetho.initializeWithDefaults(this)
     }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // multidex crash fixed
+        MultiDex.install(this)
+    }
+
 }
