a42168f3ab90060e2ccbe8feecc8ce587f3b78e3
Piyush <piyush.ibuls@gmail.com>
2020-01-19 02:06:56 +0530

    Sms Functionality added

--- app/src/main/java/com/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/dogs/view/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -138,8 +139,12 @@ class DetailFragment : Fragment() {
     }
 
 
+    //functionalty to send SMS
     private fun sendSms(smsInfo:SmsInfo){
-     //todo
+        val intent  = Intent(context,MainActivity::class.java)
+        val pi = PendingIntent.getActivity(context,0,intent,0)
+        val sms = SmsManager.getDefault()
+        sms.sendTextMessage(smsInfo.to,null,smsInfo.text,pi,null)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
